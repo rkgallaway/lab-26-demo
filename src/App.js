180db197aca0a109dffc33a9bcdc0e75e957f6c2
Ryan Gallaway <rkgallaway@msn.com>
2023-01-02 11:59:19 -0800

    demo complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Article from './Components/Article';
+import Content from './Components/Content';
+import Header from './Components/Header';
+
+class App extends React.Component {
+
+  changeTitle = (title) => {
+    document.title = title;
+  };
+
+  render(){
+    return (
+      <>
+        <Header greeting="World" />
+        <Content changeTitle={this.changeTitle}/>
+        <Article />
+      </>
+    )
+  }
 }
 
 export default App;
